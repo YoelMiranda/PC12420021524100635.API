3dc80c7df6c5d5be49f5231f5e73d00fc064f274
Yoel Miranda <24200215@ue.edu.pe>
2026-05-28 19:07:59 -0500

    controller tiposervico

---
+++ PC12420021524100635.API/Controllers/TipoServicioController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PC12420021524100635.CORE.Core;
+using PC12420021524100635.CORE.Infrastructure.Data;
+
+namespace PC12420021524100635.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoServicioController : ControllerBase
+    {
+        public readonly TallerMecanicoDbContext _context;
+
+        public TipoServicioController(TallerMecanicoDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TipoServicio
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TipoServicio>>> GetTipoServicios()
+        {
+            return await _context.TipoServicios.ToListAsync();
+        }
+
+        // GET: api/TipoServicio/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TipoServicio>> GetTipoServicio(int id)
+        {
+            var tipoServicio = await _context.TipoServicios.FindAsync(id);
+
+            if (tipoServicio == null)
+            {
+                return NotFound();
+            }
+
+            return tipoServicio;
+        }
+
+        // PUT: api/TipoServicio/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTipoServicio(int id, TipoServicio tipoServicio)
+        {
+            if (id != tipoServicio.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tipoServicio).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TipoServicioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TipoServicio
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TipoServicio>> PostTipoServicio(TipoServicio tipoServicio)
+        {
+            _context.TipoServicios.Add(tipoServicio);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTipoServicio", new { id = tipoServicio.Id }, tipoServicio);
+        }
+
+        // DELETE: api/TipoServicio/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTipoServicio(int id)
+        {
+            var tipoServicio = await _context.TipoServicios.FindAsync(id);
+            if (tipoServicio == null)
+            {
+                return NotFound();
+            }
+
+            _context.TipoServicios.Remove(tipoServicio);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TipoServicioExists(int id)
+        {
+            return _context.TipoServicios.Any(e => e.Id == id);
+        }
+    }
+}
